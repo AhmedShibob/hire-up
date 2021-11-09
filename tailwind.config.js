b28131148b58bce8954ad49f8cbcefa36d6ff57e
Ashibob <Ashibob.yousef@gmail.com>
2021-11-09 03:42:33 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      boxShadow: {
+       
+        lg: '0 0 50px rgba(209,209,209, 0.99);',
+       
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
